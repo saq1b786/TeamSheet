4e0ee948664ded3a04988c19521c7849bd21fc8e
Saqib Rahman <sqbrahman17@gmail.com>
2026-06-22 22:53:02 +0100

    feat: created some tables using sqlalchemy

---
+++ backend/database.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy import create_engine
+
+class Base(DeclarativeBase):
+    pass
+
+
+engine = create_engine("sqlite:///teamsheet.db")
+SessionLocal = sessionmaker(bind=engine)
+
+class Player(Base):
+    __tablename__ = 'players'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    phone_number = Column(String, nullable=False)
+    is_admin = Column(Boolean, default=False)
+    tallies = Column(Integer, default=0)
+    consecutive_clean_weeks = Column(Integer, default=0)
+
+class GameSession(Base):
+    __tablename__ = 'sessions'
+
+    id = Column(Integer, primary_key=True)
+    location = Column(String, nullable=True)
+    pitch_cost = Column(Float, nullable=False)
+    date = Column(String, nullable=False)
+    time = Column(String, nullable=False)
+
+class RSVP(Base):
+    __tablename__ = 'rsvp'
+
+    id = Column(Integer, primary_key=True)
+    player_id = Column(Integer, ForeignKey('players.id'))
+    session_id = Column(Integer, ForeignKey('sessions.id'))
+    is_coming = Column(Boolean, nullable=False)
